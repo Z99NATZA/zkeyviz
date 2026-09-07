7e8d3b7db2609802241ec932ecdafe09bf2ee7c1
Z99NATZA <z99natza@gmail.com>
2026-09-07 23:41:49 +0700

    feat: track pressed keys

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use evdev::{Device, EventSummary, InputEvent};
-use std::{env, error::Error};
+use evdev::{Device, EventSummary, InputEvent, KeyCode};
+use std::{collections::HashSet, env, error::Error};
 
 const DEFAULT_DEVICE: &str = "/dev/input/by-id/usb-ROYUAN_Gaming_keyboard-event-kbd";
 
@@ -19,10 +19,20 @@ fn key_state(value: i32) -> Option<KeyState> {
     }
 }
 
-fn handle_event(event: InputEvent) {
+fn handle_event(event: InputEvent, pressed_keys: &mut HashSet<KeyCode>) {
     if let EventSummary::Key(_, key, value) = event.destructure() {
         if let Some(state) = key_state(value) {
-            println!("{key:?} -> {state:?}");
+            match state {
+                KeyState::Pressed => {
+                    pressed_keys.insert(key);
+                }
+                KeyState::Released => {
+                    pressed_keys.remove(&key);
+                }
+                KeyState::Repeated => {}
+            }
+
+            println!("{pressed_keys:?}");
         }
     }
 }
@@ -34,13 +44,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut device = Device::open(&path)?;
     let name = device.name().unwrap_or("Unknown device");
+    let mut pressed_keys = HashSet::new();
 
     println!("Using keyboard: {name}");
     println!("Device path: {path}");
 
     loop {
         for event in device.fetch_events()? {
-            handle_event(event);
+            handle_event(event, &mut pressed_keys);
         }
     }
 }
